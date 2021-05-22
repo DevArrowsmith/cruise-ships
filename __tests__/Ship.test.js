2ac5df2d1c3fe22c92aae5f4762a71a64ab18e15
devarrowsmith <devarrowsmith@gmail.com>
2021-05-22 14:32:26 +0100

    Refactor Ship.test.js.

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -1,14 +1,16 @@
 const Ship = require("../src/Ship");
 
 describe("Ship", () => {
+    beforeEach(() => {
+        ship = new Ship("Lisbon");
+    });
     it("returns an object", () => {
-        expect(new Ship()).toBeInstanceOf(Object);
+        expect(ship).toBeInstanceOf(Object);
     });
     it("is created with a specified 'currentPort' value", () => {
-        expect(new Ship("Lisbon").currentPort).toBe("Lisbon");
+        expect(ship.currentPort).toBe("Lisbon");
     });
     it("can set sail", () => {
-        const ship = new Ship("Lisbon");
         ship.setSail();
         expect(ship.currentPort).toBeFalsy();
     });
