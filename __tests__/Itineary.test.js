d51187c294fa8cf32d68786090a4e8fbc14c033f
devarrowsmith <devarrowsmith@gmail.com>
2021-05-22 16:16:07 +0100

    Create test Itineary: has a 'ports' property containing an array of all arguments.

--- __tests__/Itineary.test.js
+++ __tests__/Itineary.test.js
@@ -4,4 +4,9 @@ describe("Itineary", () => {
     it("returns an object", () => {
         expect(new Itineary()).toBeInstanceOf(Object);
     });
+    it("has a 'ports' property containing an array of all arguments", () => {
+        itineary = new Itineary({}, {}, {});
+        expect(Array.isArray(itineary.ports)).toBe(true);
+        expect(itineary.ports.length).toBe(3);
+    });
 });
--- src/Itineary.js
+++ src/Itineary.js
@@ -1,6 +1,5 @@
 class Itineary {
     constructor () {
-
     };
 };
 
