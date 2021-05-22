59f7f3c9556cdea54b7bf14cbce6ddc1bbba36b1
devarrowsmith <devarrowsmith@gmail.com>
2021-05-22 14:23:35 +0100

    Update Ship constructor to pass the test 'can set sail'.

--- src/Ship.js
+++ src/Ship.js
@@ -2,4 +2,10 @@ function Ship (startingPort) {
     this.startingPort = startingPort;
 };
 
+Ship.prototype = {
+    setSail () {
+        this.startingPort = undefined;
+    }
+}
+
 module.exports = Ship;
