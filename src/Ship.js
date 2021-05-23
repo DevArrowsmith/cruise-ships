35fc22daeba7ec97d82a7f392111955ea263fbc2
devarrowsmith <devarrowsmith@gmail.com>
2021-05-24 00:18:38 +0100

    Update Ship class to pass the test 'returns an error when the 'dock' method is called and 'currentPort' is not undefined'.

--- src/Ship.js
+++ src/Ship.js
@@ -10,6 +10,9 @@ class Ship {
         this.currentPort = undefined;
     };
     dock() {
+        if(this.currentPort !== undefined) {
+            return `The ship is already docked at ${this.currentPort}. Please set sail before attempting to dock.`;
+        };
         this.currentPortNumber += 1;
         this.currentPort = this.itineary.ports[this.currentPortNumber];
     };
