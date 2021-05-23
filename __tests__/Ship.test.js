5b9ec4352c77712cb1a4de1a0a70fd400fabeabe
devarrowsmith <devarrowsmith@gmail.com>
2021-05-24 00:19:58 +0100

    Refactored Ship.test.js.

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -15,7 +15,7 @@ describe("Ship", () => {
     it("has an initial 'currentPort' value equal to the value of the first element in the 'ports' array of the 'itineary' property", () => {
         expect(ship.currentPort).toBe(ship.itineary.ports[0]);
     });
-    it("can set sail", () => {
+    it("can set sail when currentPort is not undefined", () => {
         ship.setSail();
         expect(ship.currentPort).toBeFalsy();
     });
