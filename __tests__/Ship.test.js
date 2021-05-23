49ceda41b149ab2a2c347ab4b0aad1862303b74c
devarrowsmith <devarrowsmith@gmail.com>
2021-05-23 23:21:25 +0100

    Create test: Ship: has an initial 'currentPort' value equal to the value of the first element in the 'ports' array of the 'itineary' property.

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -2,13 +2,18 @@ const Ship = require("../src/Ship");
 
 describe("Ship", () => {
     beforeEach(() => {
-        ship = new Ship({});
+        ship = new Ship({
+            ports: ["lisbon", "valencia"]
+        });
     });
     it("returns an object", () => {
         expect(ship).toBeInstanceOf(Object);
     });
     it("is created with a specified object as the 'itineary' value", () => {
         expect(ship.itineary).toBeInstanceOf(Object);
+    });
+    it("has an initial 'currentPort' value equal to the value of the first element in the 'ports' array of the 'itineary' property", () => {
+        expect(ship.currentPort).toBe(ship.itineary.ports[0]);
     });
     it("can set sail", () => {
         ship.setSail();
