dded74b6127ee8bb95487afca1fab6a3bad47e66
gummicode <gummicodeblog@gmail.com>
2021-05-24 12:41:29 +0100

    Refactor Itineary.test.js.

--- __tests__/Itineary.test.js
+++ __tests__/Itineary.test.js
@@ -5,8 +5,15 @@ describe("Itineary", () => {
         expect(new Itineary()).toBeInstanceOf(Object);
     });
     it("has a 'ports' property containing an array of all arguments", () => {
-        itineary = new Itineary({}, {}, {});
-        expect(Array.isArray(itineary.ports)).toBe(true);
-        expect(itineary.ports.length).toBe(3);
+        const port1 = { 
+            name: "Lisbon"
+        };
+        const port2 = {
+            name: "Valencia"
+        };
+        itineary = new Itineary(port1, port2);
+        expect(itineary).toEqual({
+            ports: [{name: "Lisbon"}, {name: "Valencia"}]
+        });
     });
 });
