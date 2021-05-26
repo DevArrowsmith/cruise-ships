5a4945fe5e7fa1d01c73f774dd2248940b3787dc
devarrowsmith <devarrowsmith@gmail.com>
2021-05-26 22:21:32 +0100

    Create test: Port: is created with a 'ships' value of an empty array.

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -10,4 +10,8 @@ describe("Port", () => {
     it("is created with a specified 'name' value", () => {
         expect(port.name).toBe("Lisbon")
     })
+    it("is created with a 'ships' value of an empty array", () => {
+        expect(Array.isArray(port.ships)).toBe(true);
+        expect(port.ships.length).toBe(0);
+    })
 });
