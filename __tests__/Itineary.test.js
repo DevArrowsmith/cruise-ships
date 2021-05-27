c41777623072ff2819c6f1a258852a361a27b9ad
devarrowsmith <devarrowsmith@gmail.com>
2021-05-27 21:00:01 +0100

    Refactor Itineary.test.js to use dummies in place of unneccessary objects.

--- __tests__/Itineary.test.js
+++ __tests__/Itineary.test.js
@@ -5,15 +5,8 @@ describe("Itineary", () => {
         expect(new Itineary()).toBeInstanceOf(Object);
     });
     it("has a 'ports' property containing an array of all arguments", () => {
-        const port1 = { 
-            name: "Lisbon"
-        };
-        const port2 = {
-            name: "Valencia"
-        };
-        itineary = new Itineary(port1, port2);
-        expect(itineary).toEqual({
-            ports: [{name: "Lisbon"}, {name: "Valencia"}]
-        });
+        const itineary = new Itineary(jest.fn(), jest.fn());
+        expect(Array.isArray(itineary.ports)).toBe(true);
+        expect(itineary.ports.length).toBe(2);
     });
 });
