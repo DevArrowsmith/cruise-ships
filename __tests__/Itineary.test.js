344f715aea8389cda163a4e293bbf69da144399e
devarrowsmith <devarrowsmith@gmail.com>
2021-05-31 21:05:21 +0100

    Refactor Itineary.test.js.

--- __tests__/Itineary.test.js
+++ __tests__/Itineary.test.js
@@ -1,11 +1,13 @@
 const Itineary = require("../src/Itineary");
 
 describe("Itineary", () => {
+    beforeEach(() => {
+        itineary = new Itineary(jest.fn(), jest.fn());
+    });
     it("returns an object", () => {
-        expect(new Itineary()).toBeInstanceOf(Object);
+        expect(itineary).toBeInstanceOf(Object);
     });
     it("has a 'ports' property containing an array of all arguments", () => {
-        const itineary = new Itineary(jest.fn(), jest.fn());
         expect(Array.isArray(itineary.ports)).toBe(true);
         expect(itineary.ports.length).toBe(2);
     });
