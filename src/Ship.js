1cb2a87b751f40439d1dd5c7d3723d71547caaca
devarrowsmith <devarrowsmith@gmail.com>
2021-06-01 20:59:09 +0100

    Update Ship.js. Encapsulate Ship class in an IIFE. Conditionally export Ship (node) or attach Ship to the window object (browser).

--- src/Ship.js
+++ src/Ship.js
@@ -1,30 +1,36 @@
-const INITIAL_PORT_NUMBER = 0;
+(function exportShip() {
+    const INITIAL_PORT_NUMBER = 0;
 
-class Ship {
-    constructor(itineary) {
-        this.itineary = itineary;
-        this.currentPortNumber = INITIAL_PORT_NUMBER;
-        this.currentPort = this.itineary.ports[this.currentPortNumber];
-        this.currentPort.addShip(this);
-    };
-    setSail() {
-        if(this.currentPort === undefined) {
-            return "The ship is already at sea. Please dock before attempting to set sail.";
+    class Ship {
+        constructor(itineary) {
+            this.itineary = itineary;
+            this.currentPortNumber = INITIAL_PORT_NUMBER;
+            this.currentPort = this.itineary.ports[this.currentPortNumber];
+            this.currentPort.addShip(this);
         };
-        if(this.currentPortNumber === this.itineary.ports.length - 1) {
-            return "The ship has reached its final destination and cannot currently set sail.";
+        setSail() {
+            if(this.currentPort === undefined) {
+                return "The ship is already at sea. Please dock before attempting to set sail.";
+            };
+            if(this.currentPortNumber === this.itineary.ports.length - 1) {
+                return "The ship has reached its final destination and cannot currently set sail.";
+            };
+            this.currentPort.removeShip(this);
+            this.currentPort = undefined;
         };
-        this.currentPort.removeShip(this);
-        this.currentPort = undefined;
-    };
-    dock() {
-        if(this.currentPort !== undefined) {
-            return `The ship is already docked at ${this.currentPort.name}. Please set sail before attempting to dock.`;
+        dock() {
+            if(this.currentPort !== undefined) {
+                return `The ship is already docked at ${this.currentPort.name}. Please set sail before attempting to dock.`;
+            };
+            this.currentPortNumber += 1;
+            this.currentPort = this.itineary.ports[this.currentPortNumber];
+            this.currentPort.addShip(this);
         };
-        this.currentPortNumber += 1;
-        this.currentPort = this.itineary.ports[this.currentPortNumber];
-        this.currentPort.addShip(this);
     };
-};
 
-module.exports = Ship;
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Ship;
+    } else {
+        window.Ship = Ship;
+    };
+}());
