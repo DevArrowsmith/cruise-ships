997c33745f1bfe4a196d41be11931f8d73fe121f
devarrowsmith <devarrowsmith@gmail.com>
2021-06-01 20:52:15 +0100

    Update Controller.js. Encapsulate Controller class in an IIFE. Conditionally export Controller (node) or attach Controller to the window object (browser).

--- src/Controller.js
+++ src/Controller.js
@@ -1,15 +1,23 @@
-class Controller {
-    constructor () {
+(function exportController() {
+    class Controller {
+        constructor () {
+        };
+        initialiseSea() {
+            const backgrounds = [
+                "./assets/water1.png",
+                "./assets/water0.png"
+            ]
+            let backgroundIndex = 0;
+            window.setInterval(() => {
+                document.querySelector('#viewport').style.backgroundImage = `url("${backgrounds[backgroundIndex % backgrounds.length]}")`;
+                backgroundIndex += 1;
+            }, 1000);
+        }
     };
-    initialiseSea() {
-        const backgrounds = [
-            "./assets/water1.png",
-            "./assets/water0.png"
-        ]
-        let backgroundIndex = 0;
-        window.setInterval(() => {
-            document.querySelector('#viewport').style.backgroundImage = `url("${backgrounds[backgroundIndex % backgrounds.length]}")`;
-            backgroundIndex += 1;
-        }, 1000);
-    }
-}
+
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Controller;
+    } else {
+        window.Controller = Controller;
+    };
+}());
